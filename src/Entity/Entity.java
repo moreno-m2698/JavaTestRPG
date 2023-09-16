23ed013afb2fa55e00b3be9bdbe7be7f17cdc920
Michael <moreno.m2698@gmail.com>
2023-09-16 14:59:09 -0400

    Added entity constructor

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -8,5 +8,13 @@ public class Entity
     int defense;
     String name;
 
+    public Entity(int health, int mana, int attack, int defense, String name) {
+        this.health = health;
+        this.mana = mana;
+        this.attack = attack;
+        this.defense = defense;
+        this.name = name;
+    }
+
 
 }
