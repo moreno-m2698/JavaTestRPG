389d8e2d5d8dad1c5be9630e01a6b11e4fd3961e
Michael <moreno.m2698@gmail.com>
2023-09-16 15:57:14 -0400

    populated managers with entities

--- src/Entity/HeroClassManager.java
+++ src/Entity/HeroClassManager.java
@@ -1,6 +1,8 @@
 package Entity;
 
 public class HeroClassManager {
-    Entity mage;
-    Entity knight;
+
+    public static final Entity MAGE= new Entity(100, 30, 14, 1, "Mage");
+    public static final Entity KNIGHT = new Entity(115, 15, 17, 13, "Knight");
+
 }
--- src/Entity/MonsterManager.java
+++ src/Entity/MonsterManager.java
@@ -1,4 +1,8 @@
 package Entity;
 
 public class MonsterManager {
+    //Constructor health, mana, attack, defense, name
+    public static final Entity BLUE_SLIME = new Entity(25, 0, 7, 2, "Blue Slime");
+    public static final Entity GREEN_SLIME = new Entity(14,0,6,0,"Green Slime");
+
 }
